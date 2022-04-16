76058d289def5cd38ffb764e9c5eea250aa9f4d0
mroble3 <88886554+mroble3@users.noreply.github.com>
2022-04-16 06:41:10 -0400

    Create networkStudent.cpp

---
+++ networkStudent.cpp
@@ -0,0 +1,46 @@
+// file name networkStudent.cpp 
+// Author Mohamed Roble
+// Date 09/12/2019
+#include <iostream>
+#include<iomanip>
+#include "networkstudent.h"
+using std::cout;
+using std::left;
+using std::setw;
+
+NetworkStudent::NetworkStudent():Student()
+{
+	//This is the only work this constructor needs to do; the Student class will do the rest; NEAT, HUH?
+	dtype = NETWORK;
+}
+
+NetworkStudent::NetworkStudent(string StudentID, string firstName, string lastName, string emailAddress, int age, int days[], DegreeType degreetype)
+	:Student(StudentID, firstName, lastName, emailAddress, age, days)
+{
+	dtype = NETWORK;
+}
+
+DegreeType NetworkStudent::getDegreeType()
+{
+	return NETWORK;//Now we can do this - it's not virtual anymore
+}
+
+void NetworkStudent::print()
+{
+	cout << left << setw(5) << getStudentID();
+	cout << left << setw(15) << getfirstName();
+	cout << left << setw(20) << getlastName();
+	cout << left << setw(30) << getemailAddress();
+	cout << left << setw(5) << getage();
+	cout << left << setw(10) << getDays()[0];
+	cout << left << setw(10) << getDays()[1];
+	cout << left << setw(10) << getDays()[2];
+	//this->Student::print();//Call the print method from the Student class
+	cout << "NETWORK" << "\n";//This is the only thing left to display
+}
+
+NetworkStudent::~NetworkStudent()
+{
+	Student::~Student();//Call the super destructor from Student in case it needs to release something
+	//Note: objects should not destroy themselves; only what they dynamically create for themselves
+}
